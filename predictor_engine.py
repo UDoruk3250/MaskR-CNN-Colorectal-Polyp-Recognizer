2c35a486aeab77cc1d049387a48946a02908f907
Doruk Alp <duzunarslan27@my.uaa.k12.tr>
2026-06-29 01:59:24 +0300

    Upload predictor_engine.py
    This class initializes the Detectron2 model with the necessary configurations and provides a method to make predictions on input images.

---
+++ predictor_engine.py
@@ -0,0 +1,24 @@
+import torch
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+
+class DetectronPredictorEngine:
+    def __init__(self):
+        """
+        Initializes the Detectron2 model with configuration setups.
+        """
+        self.cfg = get_cfg()
+        self.cfg.MODEL.DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+        
+        self.cfg.merge_from_file(
+            model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+        )
+        self.MODEL.WEIGHTS = "model_final.pth"
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
+        
+        self.predictor = DefaultPredictor(self.cfg)
+
+    def __call__(self, image):
+        return self.predictor(image)
