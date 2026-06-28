b5e6125b725012d93f569ce42c68ce7c37b837ff
Doruk Alp <duzunarslan27@my.uaa.k12.tr>
2026-06-29 02:03:12 +0300

    Upload predictor_engine.py

--- predictor_engine.py
+++ predictor_engine.py
@@ -5,15 +5,13 @@ from detectron2 import model_zoo
 
 class DetectronPredictorEngine:
     def __init__(self):
-        """
-        Initializes the Detectron2 model with configuration setups.
-        """
         self.cfg = get_cfg()
         self.cfg.MODEL.DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
         
         self.cfg.merge_from_file(
             model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
         )
+        
         self.MODEL.WEIGHTS = "model_final.pth"
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
         self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
